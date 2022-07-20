c38cd5ef6b67d664e6a0a38cf0260ca210933e3c
KaneGreen <737445366KG@Gmail.com>
2022-07-20 16:16:50 +0800

    try fix build

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -2,35 +2,35 @@
 set -e
 alias wget="$(which wget) --https-only --retry-connrefused"
 
-# get the latest release version of 21.02
-LATESTRELEASE=$(curl -sSf -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/openwrt/openwrt/tags | jq '.[].name' | grep -v 'rc' | grep 'v21' | sort -r | head -n 1)
-LATESTRELEASE=${LATESTRELEASE:1:-1}
-
-wget https://github.com/openwrt/openwrt/archive/${LATESTRELEASE}.tar.gz
-
-mkdir openwrt_release
-
-tar xf ${LATESTRELEASE}.tar.gz --strip-components=1 --directory=./openwrt_release
-rm  -f ${LATESTRELEASE}.tar.gz
+## get the latest release version of 21.02
+#LATESTRELEASE=$(curl -sSf -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/openwrt/openwrt/tags | jq '.[].name' | grep -v 'rc' | grep 'v21' | sort -r | head -n 1)
+#LATESTRELEASE=${LATESTRELEASE:1:-1}
+#
+#wget https://github.com/openwrt/openwrt/archive/${LATESTRELEASE}.tar.gz
+#
+#mkdir openwrt_release
+#
+#tar xf ${LATESTRELEASE}.tar.gz --strip-components=1 --directory=./openwrt_release
+#rm  -f ${LATESTRELEASE}.tar.gz
 
 git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt.git
-rm  -f ./openwrt/include/version.mk
-rm  -f ./openwrt/include/kernel.mk
-rm  -f ./openwrt/include/kernel-version.mk
-rm  -f ./openwrt/include/toolchain-build.mk
-rm  -f ./openwrt/include/kernel-defaults.mk
-rm  -f ./openwrt/package/base-files/image-config.in
-rm -rf ./openwrt/target/linux/
-
-cp  -f ./openwrt_release/include/version.mk                 ./openwrt/include/version.mk
-cp  -f ./openwrt_release/include/kernel.mk                  ./openwrt/include/kernel.mk
-cp  -f ./openwrt_release/include/kernel-version.mk          ./openwrt/include/kernel-version.mk
-cp  -f ./openwrt_release/include/toolchain-build.mk         ./openwrt/include/toolchain-build.mk
-cp  -f ./openwrt_release/include/kernel-defaults.mk         ./openwrt/include/kernel-defaults.mk
-cp  -f ./openwrt_release/package/base-files/image-config.in ./openwrt/package/base-files/image-config.in
-cp -rf ./openwrt_release/target/linux/                      ./openwrt/target/linux/
-
-rm -rf ./openwrt_release/
+#rm  -f ./openwrt/include/version.mk
+#rm  -f ./openwrt/include/kernel.mk
+#rm  -f ./openwrt/include/kernel-version.mk
+#rm  -f ./openwrt/include/toolchain-build.mk
+#rm  -f ./openwrt/include/kernel-defaults.mk
+#rm  -f ./openwrt/package/base-files/image-config.in
+#rm -rf ./openwrt/target/linux/
+#
+#cp  -f ./openwrt_release/include/version.mk                 ./openwrt/include/version.mk
+#cp  -f ./openwrt_release/include/kernel.mk                  ./openwrt/include/kernel.mk
+#cp  -f ./openwrt_release/include/kernel-version.mk          ./openwrt/include/kernel-version.mk
+#cp  -f ./openwrt_release/include/toolchain-build.mk         ./openwrt/include/toolchain-build.mk
+#cp  -f ./openwrt_release/include/kernel-defaults.mk         ./openwrt/include/kernel-defaults.mk
+#cp  -f ./openwrt_release/package/base-files/image-config.in ./openwrt/package/base-files/image-config.in
+#cp -rf ./openwrt_release/target/linux/                      ./openwrt/target/linux/
+#
+#rm -rf ./openwrt_release/
 
 unalias wget
 exit 0
