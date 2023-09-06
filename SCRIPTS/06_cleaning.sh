1192fc7b08c99764960b68ca02df1449ff7e94bd
KaneGreen <737445366KG@Gmail.com>
2023-09-06 12:45:22 +0800

    fix R2S upload artifacts

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -2,20 +2,22 @@
 MYWORKDIR=$(mktemp -d)
 case ${MYOPENWRTTARGET} in
   R2S)
-    mv -a *squashfs* *manifest* ${MYWORKDIR}/
+    mv -f *squashfs* *manifest* ${MYWORKDIR}/
     ;;
   x86)
     mv -f *combined* *manifest* ${MYWORKDIR}/
     ;;
 esac
 rm -rf ./*
-pushd ${MYWORKDIR}
+pushd ${MYWORKDIR} > /dev/null
   gzip -d *.gz
   gzip --best --keep *.img
+  echo && echo
   sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
   md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash
+  echo && echo
   rm -f *.img
-popd
+popd  > /dev/null
 mv -f ${MYWORKDIR}/* ./
 rmdir ${MYWORKDIR}
 exit 0
